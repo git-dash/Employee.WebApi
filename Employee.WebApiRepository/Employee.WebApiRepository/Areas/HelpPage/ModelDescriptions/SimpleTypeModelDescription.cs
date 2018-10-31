32b96197631b36fb8cb34ebf14d9539df254f0e1
Dash <Dash@DASH-PC>
2018-10-31 20:39:20 +0530

    skeleton added

---
+++ Employee.WebApiRepository/Employee.WebApiRepository/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Employee.WebApiRepository.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Employee.WebApiRepository/Employee.WebApiRepository/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Employee.WebApiRepository.WebApiApplication" Language="C#" %>
